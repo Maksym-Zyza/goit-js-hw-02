7fb4ef810dc6a7e872f7df7364a4459179987c70
Maksym-Zyza <mmaksymziza@gmail.com>
2020-08-21 19:12:17 +0300

    ПЕРЕБОР МАССИВА

--- JS/theory.js
+++ JS/theory.js
@@ -7,3 +7,29 @@ console.table(lastIndex); //6   - вывод последнего индекса
 
 mass[2] = "wqwq"; //перезапись елемента массива
 console.table(mass);
+
+// ПРИСВОЕГИЕ ПО ССЫЛКЕ И ЗНАЧЕНИЮ
+// let a = 5;
+// let b = 5;
+
+// b = a;
+// a = 10;
+// console.log(a);
+// console.log(b);
+
+// let a = [1, 2, 3];
+// let b = 5;
+
+// b = a;
+// a = 10;
+// a = b;
+// console.log(a);
+// console.log(b);
+
+// ПЕРЕБОР МАССИВА
+let friends = [11, 22, 33, 44, 55, 101, 202];
+
+for (let i = 0; i <= friends.length - 1; i += 1) {
+  console.log(i); // вывод индекса
+  console.log(friends[i]); // вывод елемента
+}
