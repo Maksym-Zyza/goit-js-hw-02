b8cb8ccbaf831ac62ef896921c204933a5594f7d
Maksym-Zyza <mmaksymziza@gmail.com>
2020-09-16 09:54:47 +0300

    Зміни в задача 1

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,12 +1,25 @@
 // Задание 1
 // Напиши функцию logItems(array), которая получает массив и использует цикл for, который для каждого элемента массива будет выводить в консоль сообщение в формате[номер элемента]-[значение элемента].
 // Нумерация должна начинаться с 1. К примеру для первого элемента массива['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
-const logItems = function (array) {
-  for (let i = 0; i < array.length; i += 1) {
-    console.log(`${i + 1} - `, array[i]);
-  }
-};
+// const logItems = function (array) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     console.log(`${i + 1} - `, array[i]);
+//   }
+// };
 
+// // Вызовы функции для проверки работоспособности реализации
+// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+let result;
+const getItemsString = function (array) {
+  for (let i = 0; i < array.length; i += 1) {
+    result = `${i + 1} - ${array[i]}`;
+    result = array.join(", ");
+    // console.log(result);
+  }
+  return result;
+};
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
 // Вызовы функции для проверки работоспособности реализации
-logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
-logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+// getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+// getItemsString([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
